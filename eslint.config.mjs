1111c71fd39c55539c771f627df21b35aa039e8d
Kashish Koshti <kashishkoshti925@gmail.com>
2026-05-13 21:00:35 +0530

    Upgrade to Next.js 16 and fix deployment issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,15 @@ import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  nextVitals,
+  nextTs,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
